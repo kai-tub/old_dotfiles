42081c6d434890c5215d702109b9027afc635cb1
Kai Norman Clasen <kai.ce.tub@gmail.com>
2020-12-21 15:46:21 +0100

    fix f completion mistake

--- fish/functions/fuck.fish
+++ fish/functions/fuck.fish
@@ -2,6 +2,7 @@ function fuck -d "Correct your previous console command"
     set -l fucked_up_command $history[1]
     if ! command -q thefuck
         echo "You first need to install thefuck!"
+        exit 1
     end
     env TF_SHELL=fish TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv | read -l unfucked_command
     if [ "$unfucked_command" != "" ]
