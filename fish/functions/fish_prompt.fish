d171fb9356144e1348d82e43420b15b2183e42e9
Kai Norman Clasen <kai.ce.tub@gmail.com>
2020-12-21 09:51:37 +0100

    Added powerline-go config to bashrc

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,11 +2,13 @@ function fish_prompt
     # If command not found continue normally
     command -q powerline-go
     and eval powerline-go \
-        -newline \
         -error $status \
+        -newline \
         -shell bare \
         -modules "user,host,ssh,cwd,perms,git,hg,jobs,exit,root" \
-        -cwd-max-depth=2 \
+        -cwd-max-depth 4 \
         -hostname-only-if-ssh \
-        -cwd-mode "dironly"
+        -cwd-mode "fancy" \
+        -mode "patched"
+        # modules right needs to be configured via fish
 end
