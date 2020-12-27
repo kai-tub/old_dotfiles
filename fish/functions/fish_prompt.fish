51776f09f3f4f7f4e8404b124ffa0ac3ac4d2625
Kai Norman Clasen <kai.ce.tub@gmail.com>
2020-12-27 19:12:01 +0100

    Fix prompt if `powerline-go` is not installed

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,20 @@
 function fish_prompt
     # If command not found continue normally
-    command -q powerline-go
-    and eval powerline-go \
-        -error $status \
-        -newline \
-        -shell bare \
-        -modules "user,host,ssh,cwd,perms,git,hg,jobs,exit,root" \
-        -cwd-max-depth 4 \
-        -hostname-only-if-ssh \
-        -cwd-mode "fancy" \
-        -mode "patched"
+    if command -q powerline-go
+        eval powerline-go \
+            -error $status \
+            -newline \
+            -shell bare \
+            -modules "user,host,ssh,cwd,perms,git,hg,jobs,exit,root" \
+            -cwd-max-depth 4 \
+            -hostname-only-if-ssh \
+            -cwd-mode "fancy" \
+            -mode "patched"
+    else
+        set_color $fish_color_cwd
+        echo -n (prompt_pwd)
+        set_color normal
+        echo -n ' > '
+    end
     # modules right needs to be configured via fish
 end
