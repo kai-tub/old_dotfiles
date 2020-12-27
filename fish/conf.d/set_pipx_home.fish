5743c2cf6c0f368e2976d03e6954f01c277595b1
Kai Norman Clasen <kai.ce.tub@gmail.com>
2020-12-27 21:39:40 +0100

    Change pipx target dir
    * Fix `clean` link
    * Change pipx target_dir to `~/bin`

---
+++ fish/conf.d/set_pipx_home.fish
@@ -0,0 +1,1 @@
+set -gx PIPX_BIN_DIR $HOME/bin/
--- scripts/install_pipx_tools.sh
+++ scripts/install_pipx_tools.sh
@@ -5,6 +5,9 @@ if ! command -v pipx &> /dev/null; then
     exit 0
 fi
 
+# Also set here to ensure that it is installed to correct location
+export PIPX_BIN_DIR="$HOME/bin/"
+
 pipx ensurepath --force
 pipx install thefuck
 pipx install youtube-dl
