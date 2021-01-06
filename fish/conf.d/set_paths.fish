ac7d24bddba2bd6a92c7b8d4520d829c87d1f7f0
Kai Norman Clasen <kai.ce.tub@gmail.com>
2021-01-06 11:36:07 +0100

    typo

--- fish/conf.d/set_paths.fish
+++ fish/conf.d/set_paths.fish
@@ -1,6 +1,6 @@
 for i in $HOME/bin
     if not contains $i $PATH
         # use fish_user_paths for global activation
-        set -gx $fish_user_paths $i $fish_user_paths
+        set -gx fish_user_paths $i $fish_user_paths
     end
 end
