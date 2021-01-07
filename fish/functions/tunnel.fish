82c91065d36e9d510a12adf504998bd5821f212d
Kai Norman Clasen <kai.ce.tub@gmail.com>
2021-01-07 07:11:47 +0100

    Update tunnel function and add some words to be ignored

--- fish/functions/tunnel.fish
+++ fish/functions/tunnel.fish
@@ -1,7 +1,7 @@
 function tunnel # -a "node" "hostport" "port"
     argparse 'n/node=?' 'h/hostport=?' 'l/localport=?' -- $argv
     if test -z "$_flag_node"
-        set _flag_node "131"
+        set _flag_node "node131"
     end
     if test -z "$_flag_hostport"
         set _flag_hostport "18888"
@@ -10,7 +10,7 @@ function tunnel # -a "node" "hostport" "port"
         set _flag_localport "8888"
     end
     # cluster is defined in ssh host file
-    set cmd "ssh -NL $_flag_localport:node$_flag_node:$_flag_hostport cluster"
+    set cmd "ssh -NL $_flag_localport:$_flag_node:$_flag_hostport cluster"
     echo "$cmd"
     eval $cmd
 end
