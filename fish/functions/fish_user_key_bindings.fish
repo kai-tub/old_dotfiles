a22fc2caf5ac3b85b963cf0b24316ebc43fa55e1
Kai Norman Clasen <kai.ce.tub@gmail.com>
2021-03-08 14:52:05 +0100

    testing tmux config

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,9 +1,9 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
     if functions -q fzf_key_bindings
-	    fzf_key_bindings
+        fzf_key_bindings
     end
     for mode in insert default visual
-	bind -M $mode \co forward-char
+        bind -M $mode \co forward-char
     end
 end
