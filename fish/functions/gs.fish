ca327c10efabc2c75bc4c18c982ab6fb210bbf2a
Kai Norman Clasen <kai.ce.tub@gmail.com>
2021-06-13 19:30:49 +0200

    Updated fzf keybindings

--- fish/functions/gs.fish
+++ fish/functions/gs.fish
@@ -1,3 +1,7 @@
-function gs --description "Calls `__fzf_search_git_status`"
-    __fzf_search_git_status $argv
+function gs --description "Calls `_fzf_search_git_status`"
+    if functions -q _fzf_search_git_status
+	_fzf_search_git_status $argv
+    else
+	echo "Need to install fzf first!"
+    end
 end
